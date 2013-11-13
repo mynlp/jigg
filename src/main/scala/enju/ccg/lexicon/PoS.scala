b83be5de25e3a91239d7fcabaafebdbd00fc05b6
Hiroshi Noji <h.nouji@gmail.com>
2013-11-13 19:34:47 +0900

    refine a comment

--- src/main/scala/enju/ccg/lexicon/PoS.scala
+++ src/main/scala/enju/ccg/lexicon/PoS.scala
@@ -4,10 +4,8 @@ package enju.ccg.lexicon
  * Internal representation of Pat-of-Speech.
  * The trait gives some methods to access to the information, which might be used in some languages.
  * For example, hierar is a sequence of FineTag, which is assmed to represent hierarchy of that PoS.
- * To enable using these different types of tags transparently (it is useful in e.g., feature extractions), Conjugation or FineTag itself are PoS.
+ * To enable using these different types of tags transparently (it is useful in e.g., feature extractions), Conjugation or FineTag itself is also PoS.
  * WARNING: all PoSs have to have unique ids to be distinguished, so it assmed that surface forms of conj, hierar, and pos itself (full surface) are disjoint; if, for example, a FineTag have the same surface to a Conjugation, the dictionary discards the latter one. One solution to this problem is to add a symbol to each type of PoS, e.g., adding suffix 'F' to all FineTag instances when draw/inserting the dictionary.
- *
- * TODO: refacotor this class same as in Category classes to assign ids after once instantiating; this make using of this class more safe.
  */
 sealed trait PoS extends Numbered[String] {
   def conj:PoS = sys.error("conj is not defined in this PoS class.")
