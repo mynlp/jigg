176be10b122bbf0ae4dd07dac3c33365a14d7bdc
Hiroshi Noji <h.nouji@gmail.com>
2014-02-03 21:31:19 +0900

    Fix a bug at removing zero-weight features

--- src/main/scala/enju/ccg/Problem.scala
+++ src/main/scala/enju/ccg/Problem.scala
@@ -19,16 +19,20 @@ trait Problem {
 
   object Problem {
     def removeZeroWeightFeatures[F](indexer: FeatureIndexer[F], weights: NumericBuffer[Double]): Unit = {
+      if (indexer.size > weights.size) {
+        indexer.removeElemsOver(weights.size)
+      }
       val oldSize = indexer.size
       val removingIdxs = weights.zipWithIndex.filter(_._1 == 0).map(_._2)
       indexer.removeIndexes(removingIdxs)
       weights.removeIndexes(removingIdxs)
-      if (indexer.size != weights.size) {
-        println("indexer: " + indexer.size + " != weights: " + weights.size)
-        println("oldSize: " + oldSize)
-        println("removingIdxs size: " + removingIdxs.size)
-        println("so expected size is: " + (oldSize - removingIdxs.size))
-      }
+
+      // if (indexer.size != weights.size) {
+      //   println("indexer: " + indexer.size + " != weights: " + weights.size)
+      //   println("oldSize: " + oldSize)
+      //   println("removingIdxs size: " + removingIdxs.size)
+      //   println("so expected size is: " + (oldSize - removingIdxs.size))
+      // }
       assert(indexer.size == weights.size)
       println("feature size is reduced from " + oldSize + " -> " + indexer.size)
     }
