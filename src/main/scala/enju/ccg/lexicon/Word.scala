e4eda53fd9a9c4dd588d8c17dfbf66c438106a15
Hiroshi Noji <h.nouji@gmail.com>
2014-02-03 20:34:15 +0900

    Modify a bit Word trait

--- src/main/scala/enju/ccg/lexicon/Word.scala
+++ src/main/scala/enju/ccg/lexicon/Word.scala
@@ -3,7 +3,7 @@ package enju.ccg.lexicon
 trait Word extends Numbered[String] {
   // additional information is defined in function; may or may not be overridden in val by subclasses
   def classId:Int = throw new RuntimeException("classId is not defined in this Word class.")
-  def assignClass(classId:Int):Word
+  def assignClass(classId:Int):Word = this // default do nothing
   // some morphological information extracted from the surface form might be included ? (e.g., for morphological rich languages)
 }
 
