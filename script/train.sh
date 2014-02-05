1e3d1bb6f10dd5a4bcbf5443883d3b293eb214c7
Hiroshi Noji <h.nouji@gmail.com>
2014-02-05 17:14:43 +0900

    Fix a bug of the script

--- script/train.sh
+++ script/train.sh
@@ -2,7 +2,7 @@
 
 HEAP=100g
 
-java -Xmx${HEAP} -jar ./target/enju-ccg-assembly-0.1.jar -modelType parser -actionType train -trainPath ccgbank-20130828/devel.ccgbank -saveModelPath parser.out -numIters 10 -loadModelPath tagger.out -beam 16 -beta 0.001
+java -Xmx${HEAP} -jar ./target/enju-ccg-assembly-0.1.jar -modelType tagger -actionType train -bankDirPath ccgbank-20130828 -saveModelPath tagger.out -numIters 2 -lookupMethod surfaceAndSecondWithConj -taggerTrainAlg adaGradL1 -lambda 0.000000005 -eta 0.1
 
 java -Xmx${HEAP} -jar ./target/enju-ccg-assembly-0.1.jar -modelType parser -actionType train -bankDirPath ccgbank-20130828 -saveModelPath parser.out -numIters 40 -loadModelPath tagger.out -beam 16 -beta 0.001
 
