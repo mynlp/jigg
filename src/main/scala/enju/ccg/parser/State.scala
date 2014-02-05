8759dd4b875fc5138bc05cfc2e4ff85ddfb1dc19
Hiroshi Noji <h.nouji@gmail.com>
2014-02-05 17:07:58 +0900

    Discard the fully immutable state because it was slower than former

--- src/main/scala/enju/ccg/parser/State.scala
+++ src/main/scala/enju/ccg/parser/State.scala
@@ -68,63 +68,27 @@ case class StackedNode(item:WrappedCategory,
   // }
 }
 
-object InitialFullState extends FullState(Nil, 0, true)
+object InitialFullState extends FullState(Array.empty[StackedNode], 0, true)
 
-case class FullState(private val stack:List[StackedNode],
+case class FullState(private val stack:Array[StackedNode],
                      override val j:Int,
                      override val isGold:Boolean = false) extends State {
-  def n0 = stack match {
-    case node :: _ => Some(node)
-    case _ => None
-  }
-  def n1 = stack match {
-    case _ :: node :: _ => Some(node)
-    case _ => None
-  }
-  def n2 = stack match {
-    case _ :: _ :: node :: _ => Some(node)
-    case _ => None
-  }
-  def n3 = stack match {
-    case _ :: _ :: _ :: node :: _ => Some(node)
-    case _ => None
-  }
+  override def s0 = if (stack.size > 0) Some(stack(stack.size - 1).item) else None
+  override def s1 = if (stack.size > 1) Some(stack(stack.size - 2).item) else None
+  override def s2 = if (stack.size > 2) Some(stack(stack.size - 3).item) else None
+  override def s3 = if (stack.size > 3) Some(stack(stack.size - 4).item) else None
 
-  // These accessor cost a little. I don't override these members with vals, because
-  // the user of these accessor, i.e., feature extractor, memorize these values
-  override def s0 = n0 map { _.item }
-  override def s1 = n1 map { _.item }
-  override def s2 = n2 map { _.item }
-  override def s3 = n3 map { _.item }
+  override def s0l = if (stack.size > 0) leftCategoryIfHeadIsLeft(stack(stack.size - 1)) else None
+  override def s0r = if (stack.size > 0) rightCategoryIfHeadIsRight(stack(stack.size - 1)) else None
 
-  // override def s0 = if (stack.size > 0) Some(stack(stack.size - 1).item) else None
-  // override def s1 = if (stack.size > 1) Some(stack(stack.size - 2).item) else None
-  // override def s2 = if (stack.size > 2) Some(stack(stack.size - 3).item) else None
-  // override def s3 = if (stack.size > 3) Some(stack(stack.size - 4).item) else None
+  override def s1l = if (stack.size > 1) leftCategoryIfHeadIsLeft(stack(stack.size - 2)) else None
+  override def s1r = if (stack.size > 1) rightCategoryIfHeadIsRight(stack(stack.size - 2)) else None
 
-  override def s0l = n0 flatMap { leftCategoryIfHeadIsLeft(_) }
-  override def s0r = n0 flatMap { rightCategoryIfHeadIsRight(_) }
+  override def s0h = if (stack.size > 0) headChild(stack(stack.size - 1)) else None
+  override def s1h = if (stack.size > 1) headChild(stack(stack.size - 2)) else None
 
-  override def s1l = n1 flatMap { leftCategoryIfHeadIsLeft(_) }
-  override def s1r = n1 flatMap { rightCategoryIfHeadIsRight(_) }
-
-  override def s0h = n0 flatMap { headChild(_) }
-  override def s1h = n1 flatMap { headChild(_) }
-
-  override def s0u = n0 flatMap { childCategoryOfUnary(_) }
-  override def s1u = n1 flatMap { childCategoryOfUnary(_) }
-
-  // override def s0l = if (stack.size > 0) leftCategoryIfHeadIsLeft(stack(stack.size - 1)) else None
-  // override def s0r = if (stack.size > 0) rightCategoryIfHeadIsRight(stack(stack.size - 1)) else None
-
-  // override def s1l = if (stack.size > 1) leftCategoryIfHeadIsLeft(stack(stack.size - 2)) else None
-  // override def s1r = if (stack.size > 1) rightCategoryIfHeadIsRight(stack(stack.size - 2)) else None
-
-  // override def s0h = if (stack.size > 0) headChild(stack(stack.size - 1)) else None
-  // override def s1h = if (stack.size > 1) headChild(stack(stack.size - 2)) else None
-
-  // override def s0u = if (stack.size > 0) childCategoryOfUnary(stack(stack.size - 1)) else None
-  // override def s1u = if (stack.size > 1) childCategoryOfUnary(stack(stack.size - 2)) else None
+  override def s0u = if (stack.size > 0) childCategoryOfUnary(stack(stack.size - 1)) else None
+  override def s1u = if (stack.size > 1) childCategoryOfUnary(stack(stack.size - 2)) else None
 
   private def leftCategoryIfHeadIsLeft(node:StackedNode):Option[WrappedCategory] = if (node.isUnary) None else node match {
     case StackedNode(WrappedCategory(_,_,_,_,Left,_,_),Some(left),_) => Some(left.item)
@@ -155,16 +119,13 @@ case class FullState(private val stack:List[StackedNode],
     def doShift(category:Category) = {
       val wrappedCategory = WrappedCategory(category, "", j, category, Right, j, j + 1) // head = Right is meaningless
       val shiftingNewNode = StackedNode(wrappedCategory, None, None)
-      val newStack = shiftingNewNode :: stack
-      // val newStack = stack :+ shiftingNewNode // this data structure needs to copy the state objects here, which I hope have relatively small overhead (because stacked items are not so much)
+      val newStack = stack :+ shiftingNewNode // this data structure needs to copy the state objects here, which I hope have relatively small overhead (because stacked items are not so much)
       FullState(newStack, j + 1, actionIsGold)
     }
     def doCombine(category:Category, dir:Direction, ruleType:String) = {
-      // Direction is not intuiteive; because List is FIFO (first elem = most right)
-      val (rightNode, leftNode) = stack match {
-        case right :: left :: _ => (right, left)
-        case _ => sys.error("Combine action when stack depth = 1")
-      }
+      val leftNode = stack(stack.size - 2)
+      val rightNode = stack(stack.size - 1)
+
       val wrappedCategory = WrappedCategory(
         category,
         ruleType,
@@ -173,20 +134,16 @@ case class FullState(private val stack:List[StackedNode],
                     case Right => rightNode.item.headCategory },
         dir, leftNode.item.begin, rightNode.item.end)
       val combinedNode = StackedNode(wrappedCategory, Some(leftNode), Some(rightNode))
-
-      val newStack = combinedNode :: stack.tail.tail
-      // val newStack = stack.dropRight(1)
-      // newStack(newStack.size - 1) = combinedNode
+      val newStack = stack.dropRight(1)
+      newStack(newStack.size - 1) = combinedNode
       FullState(newStack, j, actionIsGold)
     }
     def doUnary(category:Category, ruleType:String) = {
-      val topNode = n0.get
+      val topNode = stack.last
       val wrappedCategory = topNode.item match { case a => WrappedCategory(category, ruleType, a.head, a.headCategory, a.headDir, a.begin, a.end) }
       val raisedNode = StackedNode(wrappedCategory, Some(topNode), None)
-
-      val newStack = raisedNode :: stack.tail
-      // val newStack = stack.clone
-      // newStack(newStack.size - 1) = raisedNode
+      val newStack = stack.clone
+      newStack(newStack.size - 1) = raisedNode
       FullState(newStack, j, actionIsGold)
     }
     action match {
@@ -200,9 +157,7 @@ case class FullState(private val stack:List[StackedNode],
   override def toDerivation: Derivation = {
     import enju.ccg.lexicon.{NoneChildPoint, UnaryChildPoint, BinaryChildrenPoints, Point, ChildPoint, AppliedRule}
 
-    val derivationMap = Array.fill(j + 1)(Array.fill(j + 1)(new ListMap[Category, AppliedRule]))
-    // val derivationMap = new Array[Array[ListMap[Category, AppliedRule]]](j+1)
-    // derivationMap.indices.foreach { derivationMap(_) = Array.fill(j + 1)(new ListMap[Category, AppliedRule]) }
+    val derivationMap = Array.fill(j + 1)(Array.fill(j+1)(new ListMap[Category, AppliedRule]))
     stack.foreach { _.foreachNode({
       node:StackedNode => node.item match {
         case WrappedCategory(category,ruleType,_,_,_,b,e) =>
@@ -221,7 +176,7 @@ case class FullState(private val stack:List[StackedNode],
             }
           }
       }})}
-    val roots = stack.reverse.toArray.map { node => Point(node.item.begin, node.item.end, node.item.category) }
+    val roots = stack.map { node => Point(node.item.begin, node.item.end, node.item.category) }
     Derivation(derivationMap, roots)
   }
 }
