901dfd1348eac29f7f811a152723eb9199e5259b
Hiroshi Noji <h.nouji@gmail.com>
2014-02-06 21:20:52 +0900

    Fix a bug in removing zero elements

--- src/main/scala/enju/ccg/ml/FeatureIndexer.scala
+++ src/main/scala/enju/ccg/ml/FeatureIndexer.scala
@@ -7,11 +7,9 @@ class FeatureIndexer[Feature] extends HashMap[Feature, Int] {
 
   def removeIndexes(idxs: Seq[Int]): Unit = {
     val features = this.toSeq.sortWith(_._2 < _._2).map(_._1)
-    for (i <- 0 until features.size) {
-      assert(this(features(i)) == i)
-    }
+    val originalSize = size
     (0 to idxs.size) foreach { i =>
-      val idx = if (i == idxs.size) size else idxs(i)
+      val idx = if (i == idxs.size) originalSize else idxs(i)
       val lastIdx = if (i == 0) -1 else idxs(i - 1)
       (lastIdx + 1 until idx).foreach { f => this(features(f)) -= i }
       if (i != idxs.size) this -= features(idx)
