06274092f368e5d0d554f63e6f694ca83c36a5b9
Hiroshi Noji <h.nouji@gmail.com>
2014-02-07 12:37:34 +0900

    Modify train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+beta=$1
+beam=$2
+iters=$3
+
 HEAP=100g
 
 java -Xmx${HEAP} -jar ./target/enju-ccg-assembly-0.1.jar -modelType tagger -actionType train -bankDirPath ccgbank-20130828 -saveModelPath tagger.out -numIters 15 -lookupMethod surfaceAndSecondWithConj -taggerTrainAlg adaGradL1 -lambda 0.000000005 -eta 0.1
 
-java -Xmx${HEAP} -jar ./target/enju-ccg-assembly-0.1.jar -modelType parser -actionType train -bankDirPath ccgbank-20130828 -saveModelPath parser.out -numIters 40 -loadModelPath tagger.out -beam 16 -beta 0.001
+java -Xmx${HEAP} -jar ./target/enju-ccg-assembly-0.1.jar -modelType parser -actionType train -bankDirPath ccgbank-20130828 -saveModelPath parser.out.beta=$beta.beam=$beam -numIters $3 -loadModelPath tagger.out -beam $beam -beta $beta -removeZero
 
