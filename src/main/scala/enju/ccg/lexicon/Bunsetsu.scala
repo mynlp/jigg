b244f99b09f44122bc845c368be99d0b8cf300f9
Hiroshi Noji <h.nouji@gmail.com>
2014-06-29 16:30:50 +0900

    Fix bug in the last commit

--- src/main/scala/enju/ccg/lexicon/Bunsetsu.scala
+++ src/main/scala/enju/ccg/lexicon/Bunsetsu.scala
@@ -80,8 +80,6 @@ case class ParsedBunsetsuSentence(
 
   def renderInCabocha: String = headSeq.zipWithIndex.zip(bunsetsuSeq).map { case ((h, i), bunsetsu) =>
     val depStr = "* " + i + " " + h + "D"
-    val bunsetsuStr = (0 until bunsetsu.size).toList.map { i => bunsetsu.word(i) + "\t" + bunsetsu.pos(i) }
-
-    Seq(depStr, bunsetsuStr, "EOS").mkString("\n")
-  }.mkString("\n")
+    (depStr :: (0 until bunsetsu.size).toList.map { i => bunsetsu.word(i) + "\t" + bunsetsu.pos(i) }).mkString("\n")
+  }.mkString("\n") + "\nEOS"
 }
