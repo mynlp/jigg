ea77733c2849b12f24c1f238056728a96dd4f421
Hiroshi Noji <h.nouji@gmail.com>
2014-07-25 12:51:51 +0900

    CCGBank to Enju derivation uses the method of Derivation

--- src/main/scala/enju/ccg/CCGBank2EnjuXML.scala
+++ src/main/scala/enju/ccg/CCGBank2EnjuXML.scala
@@ -29,35 +29,18 @@ object CCGBank2EnjuXML {
       val conv = new JapaneseParseTreeConverter(dict)
 
       val reader = new CCGBankReader(dict)
-      val parseFragments: Seq[Seq[Tree]] =
+
+      val instances: Seq[(TaggedSentence, Derivation)] =
         reader.takeLines(Opt.ccgBankPath, Opt.numSentences).toSeq.map { line =>
-          reader.readParseFragments(line).map { conv.toLabelTree(_) }
+          val trees = reader.readParseFragments(line).map { conv.toLabelTree(_) }
+          (conv.toSentenceFromLabelTrees(trees), conv.toFragmentalDerivation(trees))
         }
 
       val fw = new FileWriter(Opt.outputPath)
 
-      parseFragments.zipWithIndex foreach { case (t, i) => fw.write(xmlFormat(t, i) + "\n") }
+      instances.zipWithIndex foreach { case ((s, d), i) => fw.write(d.renderEnjuXML(s, i) + "\n") }
 
       fw.flush
       fw.close
     }
-
-    def xmlFormat(trees: Seq[Tree], i: Int): String =
-      s"""<sentence id="s$i" parse_status="success">""" + trees.map { xmlFormat(_) }.mkString + "</sentence>"
-
-    def xmlFormat(tree: Tree): String = tree match {
-      case UnaryTree(child, label) =>
-        cons(label) + xmlFormat(child) + "</cons>"
-      case BinaryTree(left, right, label) =>
-        cons(label) + xmlFormat(left) + xmlFormat(right) + "</cons>"
-      case LeafTree(label: TerminalLabel) => cons(label) + tok(label) + "</cons>"
-      case _ => sys.error("A leaf node with a non-terminal label found.")
-    }
-    def removeFeatureKeys(category: String) = {
-      val r = """([^\[,]\w+)=""".r
-      r.replaceAllIn(category, "")
-    }
-    def cons(label: NodeLabel) = s"""<cons id="c${label.category.id}" cat="${removeFeatureKeys(label.category.toString)}">"""
-    def tok(label: TerminalLabel) = s"""<tok id="t${label.category.id}" surface="${label.word}" pos="${label.pos}">${label.word}</tok>"""
-  }
 }
