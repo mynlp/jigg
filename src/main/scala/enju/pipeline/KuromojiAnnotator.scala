7469271394eb6e754a476b7f3b2f4916af3048ba
Hiroshi Noji <h.nouji@gmail.com>
2014-10-10 20:09:47 +0900

    Attributes of each token annotated with kuromoji are changed

--- src/main/scala/enju/pipeline/KuromojiAnnotator.scala
+++ src/main/scala/enju/pipeline/KuromojiAnnotator.scala
@@ -5,8 +5,8 @@ import scala.io.Source
 import scala.xml._
 import collection.JavaConversions._
 
-import org.atilika.kuromoji.Token;
-import org.atilika.kuromoji.Tokenizer;
+import org.atilika.kuromoji.Token
+import org.atilika.kuromoji.Tokenizer
 
 class KuromojiAnnotator(val name: String, val props: Properties) extends SentencesAnnotator {
 
@@ -20,22 +20,29 @@ class KuromojiAnnotator(val name: String, val props: Properties) extends Sentenc
     val tokenizedSentence = tokenizer.tokenize(sentence.text)
     var tokenIndex = 0
 
-    val tokenNodes = tokenizedSentence.map { case token =>
+    val tokenNodes = tokenizedSentence.map { case token: Token =>
       val surf = token.getSurfaceForm
 
       // array of 品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
       val features = token.getAllFeaturesArray
 
-      val activePoSIndex = (1 until 4) find { features(_) == "*" } getOrElse(4)
-      val pos = features.take(activePoSIndex).mkString("-")
-
       // When attribute value is Option[xml.Text], None attribute is automatically skipped
-      val katsuyou = features(5) match {
-        case "*" => None
-        case a => Some(Text(a))
-      }
-      val base = features(6)
-      val nodes = <token id={ id(sindex, tokenIndex) } surf={ surf } pos={ pos } katsuyou={ katsuyou } base= { base } />
+      val reading = if (features.size > 8) Some(Text(features(8))) else None
+      val pronounce = if (features.size > 9) Some(Text(features(9))) else None
+
+      val nodes = <token
+        id={ id(sindex, tokenIndex) }
+        surf={ surf }
+        pos={ features(0) }
+        pos1={ features(1) }
+        pos2={ features(2) }
+        pos3={ features(3) }
+        inflectionType={ features(4) }
+        inflectionForm={ features(5) }
+        base={ features(6) }
+        reading={ reading }
+        pronounce={ pronounce }/>
+
       tokenIndex += 1
       nodes
     }
@@ -43,22 +50,6 @@ class KuromojiAnnotator(val name: String, val props: Properties) extends Sentenc
     val tokensAnnotation = <tokens>{ tokenNodes }</tokens>
 
     enju.util.XMLUtil.addChild(sentence, tokensAnnotation)
-
-    // // All parallelization should be handled here?
-    // // Can we handle token index or something in other way?
-    // val tokenizedSentences =  {
-    //   case line =>
-    // }
-
-    // var index = 0
-
-    // val sentencesNode = tokenizedSentences.map { case sentence =>
-    //   val tokenNodes = sentence.map { case token =>
-    //     index += 1
-    //     <token id={ "t" + index } surf={ token.getSurfaceForm } pos={ token.getAllFeatures }/>
-    //   }
-    //   <sentence><tokens>{ tokenNodes }</tokens></sentence>
-    // }
   }
 
   override def requires = Set(Annotator.JaSentence)
