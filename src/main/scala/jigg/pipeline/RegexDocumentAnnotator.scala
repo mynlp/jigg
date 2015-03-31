2d2cd3df59a21bdea48ecbeff124e010bd9c6d94
sakaki <takafumi.sakakibara.pdx@gmail.com>
2015-03-31 17:44:29 +0900

    Modify: use @Prop

--- src/main/scala/jigg/pipeline/RegexDocumentAnnotator.scala
+++ src/main/scala/jigg/pipeline/RegexDocumentAnnotator.scala
@@ -4,11 +4,20 @@ import java.util.Properties
 import scala.xml.Node
 
 class RegexDocumentAnnotator(override val name: String, override val props: Properties) extends Annotator {
+
+  @Prop(gloss = "Regular expression to segment documents (if omitted, specified regular expression is used)") var pattern = ""
+  readProps()
+
+  val defaultPattern = """\n{2,}"""
+  val splitRegex = pattern match{
+    case "" => defaultPattern
+    case pattern => pattern
+  }
+
   private[this] val documentIDGen = jigg.util.IDGenerator("d")
   override def annotate(annotation: Node): Node = {
     val raw = annotation.text
-    val reg = """\n{2,}"""
-    val documents = raw.split(reg).map {
+    val documents = raw.split(splitRegex).map {
       str =>
       <document id={ documentIDGen.next }>{ str }</document>
     }
