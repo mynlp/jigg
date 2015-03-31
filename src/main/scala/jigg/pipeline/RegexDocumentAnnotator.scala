17857153a71f7a5ad941d303672404eaba9316d2
sakaki <takafumi.sakakibara.pdx@gmail.com>
2015-03-31 17:04:40 +0900

    Modify: use documentIDGen

--- src/main/scala/jigg/pipeline/RegexDocumentAnnotator.scala
+++ src/main/scala/jigg/pipeline/RegexDocumentAnnotator.scala
@@ -4,21 +4,13 @@ import java.util.Properties
 import scala.xml.Node
 
 class RegexDocumentAnnotator(override val name: String, override val props: Properties) extends Annotator {
-  private[this] var documentID: Int = 0
-
+  private[this] val documentIDGen = jigg.util.IDGenerator("d")
   override def annotate(annotation: Node): Node = {
-
-    def newDocumentID(): String = {
-      val new_id = "d" + documentID
-      documentID += 1
-      new_id
-    }
-
     val raw = annotation.text
     val reg = """\n{2,}"""
     val documents = raw.split(reg).map {
       str =>
-      <document id={ newDocumentID() }>{ str }</document>
+      <document id={ documentIDGen.next }>{ str }</document>
     }
 
     <root>{ documents }</root>
