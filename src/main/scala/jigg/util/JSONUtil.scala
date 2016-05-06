dd245069991cf9c12839f517ee7c3736170e33c7
tomeken-yoshinaga <kenta-yoshinaga@aist.go.jp>
2016-05-06 08:30:01 +0000

    [add/update] JSONUtil object.
    . Add new option, `-json` to pipeline.Pipeline.
    . Modyfy following methods.
     . Pipeline.run
     . Pipeline.runFromStdin
     . Pipeline.shell

--- src/main/scala/jigg/util/JSONUtil.scala
+++ src/main/scala/jigg/util/JSONUtil.scala
@@ -1,5 +1,6 @@
 package jigg.util
 
+import java.io._
 import scala.xml._
 import scala.io.Source
 import scala.collection.mutable.StringBuilder
@@ -7,6 +8,10 @@ import scala.collection.mutable.StringBuilder
 object JSONUtil {
 
   object XMLParser {
+    // If text node has newline character, Node.child method doesn't work collectly.
+    def trimNode(x: Node): Node = 
+      XML.loadString(x.toString.split("\n").mkString)
+
     def hasChild(x: Any): Boolean = x match {
       case x: Elem => if (x.child.length > 0) true else false
       case x: List[_] if x forall (_.isInstanceOf[Node]) =>
@@ -15,7 +20,7 @@ object JSONUtil {
     }
 
     def getChildNode(x: Any): List[Node] = x match {
-      case x: Elem => (x.child filter (_.label != "#PCData")).toList
+      case x: Elem => (x.child filter (_.label != "#PCDATA")).toList
       case x: List[_] if x forall (_.isInstanceOf[Node]) =>
         (x.map(getChildNode(_))).flatten
       case _ => Nil
@@ -23,7 +28,7 @@ object JSONUtil {
 
     def getText(x: Node): String = {
       (
-        x.child.collect {
+        this.trimNode(x).child.collect {
           case Text(t) => t
         }
       ).map(_.trim).filterNot(_.isEmpty).mkString
@@ -42,35 +47,30 @@ object JSONUtil {
   }
 
   def toJSON(x: Any): StringBuilder = x match {
-    case x: String    => toJSONFromNode(XML.loadFile(x))
+    case x: String if x.endsWith(".xml")    => toJSONFromNode(XML.loadFile(x))
     case x: Elem      => toJSONFromNode(x)
     case _            => new StringBuilder("")
   }
 
-  def toJSONFromNode(node: Elem): StringBuilder = {
+  private def toJSONFromNode(node: Elem): StringBuilder = {
+    val xml = XML.loadString(node.toString.split("\n").mkString)
     val sb = new StringBuilder
 
     sb.append('{')
     sb.append(List('"',node.label,"\":").mkString)
     sb.append("{ \"_\":")
-    sb.append('[')
+    sb.append("[\n")
     
-    sb.append(serializing(node))
+    sb.append(serializing(xml))
 
-    sb.append(']')
+    sb.append("]\n")
     sb.append('}')
-    sb.append('}')
+    sb.append("}\n")
 
     sb
   }
 
-  def writeToAsJSON(x: Any, outputfile: String): Unit = {
-    val writer = IOUtil.openOut(outputfile)
-    writer.write(toJSON(x).toString)
-    writer.close()
-  }
-
-  def serializing[T <: Node](x: T): StringBuilder = {
+  private def serializing[T <: Node](x: T): StringBuilder = {
     val subsb = new StringBuilder
     if(XMLParser.hasChild(x)){
       val childNode = XMLParser.getChildNode(x)
@@ -78,19 +78,19 @@ object JSONUtil {
       for (i <- childNode){
         val retsb = serializing(i)
         subsb.append(prefix)
-        prefix = ","
+        prefix = ",\n"
         subsb.append(List("{ \"", i.label, "\":").mkString)
-        subsb.append('{')
+        subsb.append("{\n")
         var prefix2 = ""
         if(XMLParser.isTextNode(i)){
           subsb.append(prefix2)
-          prefix2 = ","
+          prefix2 = ",\n"
           subsb.append(List("\"text\":\"", XMLParser.getText(i),'"').mkString)
         }
         if (XMLParser.hasAttribution(i)){
           for(elem <- XMLParser.getAttributionList(i)){
             subsb.append(prefix2)
-            prefix2 = ","
+            prefix2 = ",\n"
             subsb.append(List('"', elem._1, "\":\"", elem._2, "\"").mkString)
           }
         }
@@ -98,14 +98,36 @@ object JSONUtil {
         if(retsb.length > 0){
           subsb.append(prefix2)
           subsb.append("\"_\":")
-          subsb.append('[')
+          subsb.append("[\n")
           subsb.append(retsb)
-          subsb.append(']')
+          subsb.append("]\n")
         }
         subsb.append('}')
         subsb.append('}')
       }
     }
     subsb
+  }
+
+  def writeToJSON[T <: Node](x: T, outputfile: String): Unit = {
+    val filePath = 
+      if (outputfile.endsWith(".json"))
+        outputfile
+      else
+        List(outputfile,".json").mkString
+    try {
+      val writer = IOUtil.openOut(outputfile)
+      writer.write(toJSON(x).toString)
+      writer.close()
+    } catch {
+      case e: FileNotFoundException => 
+        System.err.println(e.getMessage)
+      case e: IOException =>
+        System.err.println(e.getMessage)
+    }
+  }
+
+  def writeToJSON[T <: Node](x: T, os: BufferedWriter): Unit = {
+    os.write(toJSON(x).toString)
   }
 }
--- src/test/scala/jigg/util/JSONUtilTest.scala
+++ src/test/scala/jigg/util/JSONUtilTest.scala
@@ -23,7 +23,50 @@ class JSONUtiltest extends FunSuite{
       </document>
     </root>
 
-  test("toJSON generate StringBuilder object from scala.xml.Node"){
+  val textNode = 
+    <sentence id="s0" characterOffsetBegin="0" characterOffsetEnd="11">
+      Hello Jigg!
+      <tokens annotators="corenlp">
+        <token characterOffsetEnd="5" characterOffsetBegin="0" id="t0" form="Hello"/>
+        <token characterOffsetEnd="10" characterOffsetBegin="6" id="t1" form="Jigg"/>
+        <token characterOffsetEnd="11" characterOffsetBegin="10" id="t2" form="!"/>
+      </tokens>
+    </sentence>
+
+  val textNodeWithNewLine = 
+    <sentence id="s0" characterOffsetBegin="0" characterOffsetEnd="11">
+      {"Hello Jigg!\n"}
+      <tokens annotators="corenlp">
+        <token characterOffsetEnd="5" characterOffsetBegin="0" id="t0" form="Hello"/>
+        <token characterOffsetEnd="10" characterOffsetBegin="6" id="t1" form="Jigg"/>
+        <token characterOffsetEnd="11" characterOffsetBegin="10" id="t2" form="!"/>
+      </tokens>
+    </sentence>
+
+  /**
+   * Unit testing toJSON
+   */
+  test("toJSON should generate StringBuilder object from scala.xml.Node"){
     JSONUtil.toJSON(node).getClass should be (classOf[StringBuilder])
+  }
+
+  test("toJSON should return empty StringBuilder when not supported type input"){
+
+    JSONUtil.toJSON(1).length should be (0)
+    JSONUtil.toJSON(1.0).length should be (0)
+    JSONUtil.toJSON(true).length should be (0)
+    JSONUtil.toJSON('a').length should be (0)
+    JSONUtil.toJSON(List("test")).length should be (0)
+  }
+
+  /**
+   * Unit testing XMLParser 
+   */
+  test("isTextNode should be return true when a text node is input"){
+    JSONUtil.XMLParser.isTextNode(textNode) should be (true)
+  }
+
+  test("isTextNode should be return true when a text node with newline character is input"){
+    JSONUtil.XMLParser.isTextNode(textNodeWithNewLine) should be (true)
   }
 }
