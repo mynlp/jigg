8f07308041061a4e15337529c67bcc128295ed08
Hiroshi Noji <h.nouji@gmail.com>
2016-07-20 14:42:08 +0900

    Update goal path for download

--- script/download_models.sh
+++ script/download_models.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-jar=`dirname $0`/../jar
+home=`dirname $0`/..
 version=0.6.1
 
 modelurl="https://github.com/mynlp/jigg/releases/download/v-${version}/jigg-${version}-models.jar"
 
-wget $modelurl -P $jar
+wget $modelurl -P $home
