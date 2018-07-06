317c9570205160482848a8a589ca90895c074aa7
Hiroshi Noji <h.nouji@gmail.com>
2018-07-06 14:34:19 +0900

    remove deprecated scripts

--- script/download_corenlp_models.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-home=`dirname $0`/..
-
-modelurl="http://nlp.stanford.edu/software/stanford-english-corenlp-2016-01-10-models.jar"
-
-wget $modelurl -P $home
--- script/download_models.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-home=`dirname $0`/..
-version=0.6.1
-
-modelurl="https://github.com/mynlp/jigg/releases/download/v-${version}/jigg-${version}-models.jar"
-
-wget $modelurl -P $home
