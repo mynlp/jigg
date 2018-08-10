8820f5023317c4f11039c3a30188dea4e505fd61
Fumiya Yamamoto <yamamoto-fymiya10@aist.go.jp>
2018-08-10 05:35:21 +0000

    Fix script

--- .checker/scripts/install-jar.sh
+++ .checker/scripts/install-jar.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-source ./.checker/scripts/set-env.sh
-
 home_dir=`pwd ./`
 jar_dir="jar/"
 
@@ -26,7 +24,12 @@ cp ${dir}"/"${file_model} ${jar_dir}
 
 
 # create jigg jar file
-cd ${home_dir}
 jigg_file="target/jigg-assembly-0.8.0.jar"
 ./bin/sbt assembly
 cp ${jigg_file} ${jar_dir}
+
+
+# download jigg-models
+jigg_models="jigg-models.jar"
+wget https://github.com/mynlp/jigg-models/raw/master/jigg-models.jar
+mv ${jigg_models} ${jar_dir}
